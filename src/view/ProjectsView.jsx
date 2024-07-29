6f9543d11e7bd2a8bb07583f1d11d1d0144ea9c0
axel-sjostrom <axel.o.sjostrom@gmail.com>
2024-07-29 15:12:25 +0200

    bilder funkar

--- src/view/ProjectsView.jsx
+++ src/view/ProjectsView.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-
+//väldigt viktigt ifall bildernas förvaringsplats ändras att rätta till github-url:en
 function ProjectsView (props){
     const overClassName = props.overClassName;
     const underClassName = props.underClassName;
@@ -14,7 +14,7 @@ function ProjectsView (props){
                 
             <Row>
                 <Col className={underClassName}>
-                    <Image src="../pictures/awyeah.PNG" thumbnail />
+                    <Image src="https://raw.githubusercontent.com/axel-sjostrom/axel-portfolio/master/src/pictures/awyeah.PNG" thumbnail /> 
                     App Prototypes
                 </Col>
                 
